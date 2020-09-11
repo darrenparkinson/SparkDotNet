57d4928ea7740e3a56517032f8cfeb6cc9fe5422
dtibbe <david@tibbe.info>
2020-09-11 19:02:43 +0200

    API Changes of September 10th, 2020

--- APIPartials/SparkRecordings.cs
+++ APIPartials/SparkRecordings.cs
@@ -14,25 +14,44 @@ namespace SparkDotNet
         /// Lists recordings. You can specify a date range and the maximum number of recordings to return.
         /// Only recordings of meetings hosted by or shared with the authenticated user will be listed.
         /// The list returned is sorted in descending order by the date and time that the recordings were created.
+        /// If meetingId is specified, only recordings associated with the specified meeting will be listed.
         /// Long result sets are split into pages.
         /// </summary>
         /// <param name="max">Maximum number of recordings to return in a single page. max must be equal to or greater than 1 and equal to or less than 100. Default: 10</param>
         /// <param name="from">Starting date and time (inclusive) for recordings to return, in any ISO 8601 compliant format. from cannot be after current date and time or after to. Default: If `to` is specified, the default value is 7 days before `to`; if `to` is not specified, the default value is 7 days before current date and time.</param>
         /// <param name="to">Ending date and time (exclusive) for List recordings to return, in any ISO 8601 compliant format. to cannot be after current date and time or before from. Default: Current date and time</param>
+        /// <param name="meetingId">Unique identifier for the parent meeting series, scheduled meeting or meeting instance for which recordings are being requested. If a meeting series ID is specified, the operation returns an array of recordings for the specified meeting series; if a scheduled meeting ID is specified, the operation returns an array of recordings for the specified scheduled meeting; if a meeting instance ID is specified, the operation returns an array of recordings for the specified meeting instance. If not specified, the operation returns an array of recordings for all meetings of the current user.</param>
         /// <returns></returns>
-        public async Task<List<Recording>> GetRecordingsAsync(int max = 0, DateTime? from = null, DateTime? to = null)
+        public async Task<List<Recording>> GetRecordingsAsync(int max = 0, DateTime? from = null, DateTime? to = null, string meetingId = null)
         {
             var queryParams = new Dictionary<string, string>();
 
             if (max > 0) queryParams.Add("max", max.ToString());
             if (from != null) queryParams.Add("from", ((DateTime)from).ToString("o"));
             if (to != null) queryParams.Add("to", ((DateTime)to).ToString("o"));
+            if (meetingId != null) queryParams.Add("meetingId", meetingId);
 
             var path = getURL(recordingsBase, queryParams);
 
             return await GetItemsAsync<Recording>(path);
         }
 
+        /// <summary>
+        /// Lists recordings. You can specify a date range and the maximum number of recordings to return.
+        /// Only recordings of meetings hosted by or shared with the authenticated user will be listed.
+        /// The list returned is sorted in descending order by the date and time that the recordings were created.
+        /// If meetingId is specified, only recordings associated with the specified meeting will be listed.
+        /// Long result sets are split into pages.
+        /// </summary>
+        /// <param name="max">Maximum number of recordings to return in a single page. max must be equal to or greater than 1 and equal to or less than 100. Default: 10</param>
+        /// <param name="from">Starting date and time (inclusive) for recordings to return, in any ISO 8601 compliant format. from cannot be after current date and time or after to. Default: If `to` is specified, the default value is 7 days before `to`; if `to` is not specified, the default value is 7 days before current date and time.</param>
+        /// <param name="to">Ending date and time (exclusive) for List recordings to return, in any ISO 8601 compliant format. to cannot be after current date and time or before from. Default: Current date and time</param>
+        /// <param name="meeting">Meeting object for the parent meeting series, scheduled meeting or meeting instance for which recordings are being requested. If a meeting is specified, the operation returns an array of recordings for the specified meeting series; if a scheduled meeting ID is specified, the operation returns an array of recordings for the specified scheduled meeting; if a meeting instance ID is specified, the operation returns an array of recordings for the specified meeting instance. If not specified, the operation returns an array of recordings for all meetings of the current user.</param>
+        /// <returns></returns>
+        public async Task<List<Recording>> GetRecordingsAsync(int max = 0, DateTime? from = null, DateTime? to = null, Meeting meeting)
+        {
+            return await GetRecordingsAsync(max, from, to, meeting.Id);
+        }
 
         /// <summary>
         /// Retrieves details for a recording with a specified recording ID.
